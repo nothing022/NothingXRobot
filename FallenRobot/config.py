d068ee3dfd8bedcf3cf32ad8575504cd45f80bdd
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-06-29 18:40:02 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -16,7 +16,7 @@ class Config(object):
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
 
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "NothingBots"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
