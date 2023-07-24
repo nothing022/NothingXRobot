167ae2532347cc8d38f0d1cf6d45a64de4b668f8
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-07-24 20:45:40 +0530

    Update chatbot.py

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -133,7 +133,7 @@ def chatbot(update: Update, context: CallbackContext):
         if not fallen_message(context, message):
             return
         bot.send_chat_action(chat_id, action="typing")
-        url = f"https://api.safone.me/chatbot?query={message.text}&user_id={chat_id}&bot_name=Nothing_Robot&bot_master=Nothing"
+        url = f"https://fallenxbot.vercel.app/api/apikey={key}/fallen/Anonymous/message=hehe"
         request = requests.get(url)
         results = json.loads(request.text)
         sleep(0.5)
